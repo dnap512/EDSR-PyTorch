6801920f975f943708f05cfb15e24b4b562b7c03
dnap512 <frshield3@gmail.com>
2020-11-29 01:35:04 +0900

    Update srresnet.py for rsc training

--- src/model/srresnet.py
+++ src/model/srresnet.py
@@ -1,5 +1,12 @@
 from model import common
 import torch.nn as nn
+import torch
+from torch.autograd import Variable
+import numpy.random as npr
+import numpy as np
+import torch.nn.functional as F
+import random
+import math
 
 
 def make_model(args, parent=False):
@@ -43,18 +50,66 @@ class SRResNet(nn.Module):
         self.body = nn.Sequential(*m_body)
         self.tail = nn.Sequential(*m_tail)
 
-    def forward(self, x):
+        
+    def forward(self, x, flag=False, hr=None):
         x = self.sub_mean(x)
         x = self.head(x)
 
         res = self.body(x)
         res += x
-
-        x = self.tail(res)
+        
+        x = self.tail[0](res)
+        if flag:
+            self.eval()
+            x_new = x.clone().detach()
+            x_new = Variable(x_new.data, requires_grad=True).cuda()
+            num_batch, num_channel, H, W = x_new.shape
+            HW = H*W
+            
+            sr = self.tail[-1](x_new)
+            criterion = nn.L1Loss()
+            loss = criterion(sr, hr)
+            
+            self.zero_grad()
+            loss.backward()
+            grads_val = x_new.grad.clone().detach()
+            grad_channel_mean = torch.mean(grads_val.view(num_batch, num_channel, -1), dim=2)
+            channel_mean = grad_channel_mean
+            grad_channel_mean = grad_channel_mean.view(num_batch, num_channel, 1, 1)
+            spatial_mean = torch.sum(x_new * grad_channel_mean, 1)
+            spatial_mean = spatial_mean.view(num_batch, HW)
+            self.zero_grad()
+            
+            choose_one = random.randint(0,9)
+            if choose_one <= 4:
+                # ---------------------------- spatial -----------------------
+                spatial_drop_num = math.ceil(HW * 1 / 3.0)
+                th18_mask_value = torch.sort(spatial_mean, dim=1, descending=True)[0][:, spatial_drop_num]
+                th18_mask_value = th18_mask_value.view(num_batch, 1).expand(num_batch, 36864)
+                mask_all_cuda = torch.where(spatial_mean > th18_mask_value, torch.zeros(spatial_mean.shape).cuda(),
+                                            torch.ones(spatial_mean.shape).cuda())
+                mask_all = mask_all_cuda.reshape(num_batch, H, H).view(num_batch, 1, H, H)
+            else:
+                # -------------------------- channel ----------------------------
+                vector_thresh_percent = math.ceil(num_channel * 1 / 3.2)
+                vector_thresh_value = torch.sort(channel_mean, dim=1, descending=True)[0][:, vector_thresh_percent]
+                vector_thresh_value = vector_thresh_value.view(num_batch, 1).expand(num_batch, num_channel)
+                vector = torch.where(channel_mean > vector_thresh_value,
+                                     torch.zeros(channel_mean.shape).cuda(),
+                                     torch.ones(channel_mean.shape).cuda())
+                mask_all = vector.view(num_batch, num_channel, 1, 1)
+            mask_all[int(num_batch/3):,:,:,:] = 1
+            self.train()
+            mask_all = Variable(mask_all, requires_grad=True)
+            x = x * mask_all
+            
+            
+        x = self.tail[-1](x)
         x = self.add_mean(x)
 
         return x 
 
+    
     def load_state_dict(self, state_dict, strict=True):
         own_state = self.state_dict()
         for name, param in state_dict.items():
@@ -73,4 +128,3 @@ class SRResNet(nn.Module):
                 if name.find('tail') == -1:
                     raise KeyError('unexpected key "{}" in state_dict'
                                    .format(name))
-
