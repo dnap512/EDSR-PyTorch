a3d941f05ea801b398aeb8d9b01e573ac04820d0
LeeMyungHak <mh9716@naver.com>
2020-11-30 18:56:33 +0000

    Data loader

--- src/data/set14dg.py
+++ src/data/set14dg.py
@@ -1,15 +1,9 @@
 import os
 from data import srdata
 
-class DIV2K(srdata.SRData):
-    def __init__(self, args, name='DIV2K', train=True, benchmark=False):
-        if args.data_range==None:
-            file_num = len(os.listdir(os.path.join(args.dir_data,'DIV2KDG/DIV2K_train_HR'))) * len(args.use_data.split('_'))
-            split = str(int(file_num/9*8))
-            data_range = [['1', split],[str(int(split)+1), str(int(split)+10)]]
-        else:
-            data_range = [r.split('-') for r in args.data_range.split('/')]
-        
+class Set14DG(srdata.SRData):
+    def __init__(self, args, name='Set14DG', train=True, benchmark=False):
+        data_range = [r.split('-') for r in args.data_range.split('/')]
         if train:
             data_range = data_range[0]
         else:
@@ -19,20 +13,20 @@ class DIV2K(srdata.SRData):
                 data_range = data_range[1]
 
         self.begin, self.end = list(map(lambda x: int(x), data_range))
-        super(DIV2K, self).__init__(
+        super(Set14DG, self).__init__(
             args, name=name, train=train, benchmark=benchmark
         )
 
     def _scan(self):
-        names_hr, names_lr = super(DIV2K, self)._scan()
+        names_hr, names_lr = super(Set14DG, self)._scan()
         names_hr = names_hr[self.begin - 1:self.end]
         names_lr = [n[self.begin - 1:self.end] for n in names_lr]
 
         return names_hr, names_lr
 
     def _set_filesystem(self, dir_data):
-        super(DIV2K, self)._set_filesystem(dir_data)
-        self.dir_hr = os.path.join(self.apath, 'DIV2K_train_HR')
+        super(Set14DG, self)._set_filesystem(dir_data)
+        self.dir_hr = os.path.join(self.apath, 'Set14_train_HR')
         self.dir_lr = [os.path.join(self.apath, d) for d in self.args.use_data.split("_")]
         if self.input_large: self.dir_lr += 'L'
 
--- src/data/set5dg.py
+++ src/data/set5dg.py
@@ -1,15 +1,16 @@
 import os
 from data import srdata
 
-class DIV2K(srdata.SRData):
-    def __init__(self, args, name='DIV2K', train=True, benchmark=False):
+class Set5DG(srdata.SRData):
+    def __init__(self, args, name='Set5DG', train=True, benchmark=False):
         if args.data_range==None:
-            file_num = len(os.listdir(os.path.join(args.dir_data,'DIV2KDG/DIV2K_train_HR'))) * len(args.use_data.split('_'))
-            split = str(int(file_num/9*8))
-            data_range = [['1', split],[str(int(split)+1), str(int(split)+10)]]
+            file_num = len(os.listdir(os.path.join(args.dir_data,'Set5DG/Set5_train_HR'))) * len(args.use_data.split('_'))
+            split = str(int(file_num*0.9))
+            data_range = [['1', split],[str(int(split)+1), str(file_num)]]
         else:
             data_range = [r.split('-') for r in args.data_range.split('/')]
         
+        print("file_num: ",data_range)
         if train:
             data_range = data_range[0]
         else:
@@ -19,20 +20,20 @@ class DIV2K(srdata.SRData):
                 data_range = data_range[1]
 
         self.begin, self.end = list(map(lambda x: int(x), data_range))
-        super(DIV2K, self).__init__(
+        super(Set5DG, self).__init__(
             args, name=name, train=train, benchmark=benchmark
         )
 
     def _scan(self):
-        names_hr, names_lr = super(DIV2K, self)._scan()
+        names_hr, names_lr = super(Set5DG, self)._scan()
         names_hr = names_hr[self.begin - 1:self.end]
         names_lr = [n[self.begin - 1:self.end] for n in names_lr]
 
         return names_hr, names_lr
 
     def _set_filesystem(self, dir_data):
-        super(DIV2K, self)._set_filesystem(dir_data)
-        self.dir_hr = os.path.join(self.apath, 'DIV2K_train_HR')
+        super(Set5DG, self)._set_filesystem(dir_data)
+        self.dir_hr = os.path.join(self.apath, 'Set5_train_HR')
         self.dir_lr = [os.path.join(self.apath, d) for d in self.args.use_data.split("_")]
         if self.input_large: self.dir_lr += 'L'
 
